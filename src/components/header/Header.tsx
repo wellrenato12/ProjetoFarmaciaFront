98a45ce130c16f11c77694a5433c6427bafd07cd
Wellington Albino <well.renato@hotmail.com>
2024-08-16 12:50:23 -0300

    Criando alguns estilos

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import { NavBar } from "../navBar/NavBar";
+import logo from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <header>
-      header
+    <header className="max-w-7xl flex items-center justify-between p-4 mx-auto">
+      <img className="w-44" src={logo} alt="logo" />
       <NavBar />
     </header>
   )
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
+import './NavBar.css'
 
 export function NavBar() {
   return (
     <div className="flex gap-4 mb-4">
-      <h1>NavBar</h1>
-      <Link to="/home">Home</Link>
+      <ul className="flex gap-4 relative menu">
+        <Link to="/home">
+          <li style={{  }} className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Home</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Produtos</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Sobre</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Contato</li>
+        </Link>
+      </ul>
     </div>
   )
 }
